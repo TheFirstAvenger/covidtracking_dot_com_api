683af1e69402f5afd8cf305b2024a3e908d837d2
TheFirstAvenger <mike@mikebinns.net>
2020-04-06 16:16:03 -0400

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CovidtrackingDotComApi.MixProject do
   def project do
     [
       app: :covidtracking_dot_com_api,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
